403083abb76c366947bbf74eb91cc7290c12730d
Kai Paterson-Hall <kai.paterson-hall@netmatters.com>
2024-02-22 11:25:43 +0000

    AddDetail action method

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -4,5 +4,9 @@ namespace ComicBookGallery.Controllers
 {
     public class ComicBooksController : Controller
     {
+        public string Detail()
+        {
+            return "<h1>Hello from the comic books  controller!</h1>";
+        }
     }
 }
---
+++ src/ComicBookGallery/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ComicBookGallery.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "Hello From Home";
+        }
+    }
+}
