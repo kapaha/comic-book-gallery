d0c0b2df1d26b56f696acb7cd0889c65e7d3169a
Kai Paterson-Hall <kai.paterson-hall@netmatters.com>
2024-02-22 12:38:10 +0000

    Update Comic Book Detail view to display comic book information

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -7,12 +7,13 @@ namespace ComicBookGallery.Controllers
         public ActionResult Detail()
         {
 
-            if (DateTime.Today.DayOfWeek == DayOfWeek.Monday)
-            {
-                return Redirect("/");
-            }
+            return View();
+            //if (DateTime.Today.DayOfWeek == DayOfWeek.Monday)
+            //{
+            //    return Redirect("/");
+            //}
 
-            return Content("Hello from the comic books controller!");
+            //return Content("Hello from the comic books controller!");
         }
     }
 }
--- src/ComicBookGallery/Program.cs
+++ src/ComicBookGallery/Program.cs
@@ -1,8 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-//builder.Services.AddControllersWithViews();
-builder.Services.AddControllers();
+builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
